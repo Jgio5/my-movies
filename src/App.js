a53128943c2ceb124e10283dd295703d85a628b7
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-10-27 15:45:42 +0200

    finish
    Creating a movie library with React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import arrow from './assets/up.png';
+import './App.scss';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import Routes from './config/Routes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="scrollTop" onClick={scrollToTop}>
+                <img src={arrow} alt="" />
+            </div>
+            <BrowserRouter className="container">
+                <Route render={props => (
+                    <>
+                        <Header {...props} />
+                        <Routes />
+                        <Footer />
+
+                    </>
+                )} />
+            </BrowserRouter> 
+        </>
+    );
 }
+
+// Funzione Fadein back to top button on page
+window.addEventListener('scroll', function() {
+    var scroll = document.querySelector('.scrollTop');
+    scroll.classList.toggle("active", window.scrollY > 500)
+})
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
 
 export default App;
